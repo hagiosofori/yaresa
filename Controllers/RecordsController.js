26c6544a4293bc4bb1da47437138e357e20c7859
alvinofori <alvin.ofori@outlook.com>
2017-01-26 17:15:38 +0000

    working on save new member method of recordsController

--- Controllers/RecordsController.js
+++ Controllers/RecordsController.js
@@ -1,2 +1,12 @@
-var createStatement;
-var 
+/**This controller is the intermediary between the Records view and the database
+ * 
+ */
+//include the database javascript file here.
+
+
+/**Function to save the details of a new member to the local database. */
+function saveNewMember(){
+    /**save the details from the fields to variables */
+
+
+}
