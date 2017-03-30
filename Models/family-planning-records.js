1deed376576085a608116545eeaefad0d85818ad
Alvin Ofori <alvin.ofori@ashesi.local>
2017-03-30 00:26:58 +0000

    creating models for member records

---
+++ Models/family-planning-records.js
@@ -0,0 +1,12 @@
+function FamilyPlanningRecord(){
+    this.id = -1;
+    this.member = '';
+    this.date = '';
+    this.quantity = -1;
+    this.familyPlanningRecords = [];
+
+    this.getAll = function(){
+        //fetch all from database and store in array.
+    }
+
+}
---
+++ Models/opd-records.js
@@ -0,0 +1,12 @@
+function OPD(){
+    this.id = -1;
+    this.name = '';
+    this.category = '';
+    this.display_order = -1;
+    this.opdCases = [];
+    //function to get all opdcases from db.
+
+    this.getAll = function(){
+        //populate array with opd objects from db.
+    }
+}
